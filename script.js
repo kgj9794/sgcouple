345ff98fed2fcc636f2622310b09ccf4bd3e0e71
김건주 <49966040+kgj9794@users.noreply.github.com>
2026-08-31 21:23:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ const KAKAO_JAVASCRIPT_KEY = "45a0a2cc0df0c3e8aac2e81b7082362a";
 // BGM 음원 목록
 const BGM_PLAYLIST = [
     "https://maplemusic.o-r.kr/%EB%85%B8%EB%9E%98/%E1%84%8B%E1%85%A6%E1%84%8B%E1%85%AE%E1%84%85%E1%85%A6%E1%86%AF.mp3",
-    "https://maplemusic.o-r.kr/%EB%85%B8%EB%9E%98/22.mp3"
+    "https://maplemusic.o-r.kr/%EB%85%B8%EB%9E%98/22.mp3",
+    "https://maplemusic.o-r.kr/%EB%85%B8%EB%9E%98/30.mp3"
 ];
 
 // 섹션 11 야경 배경 이미지 목록
